643f8fc8ebe23ea107ba15681653272d655a259a
LJ-Fryer <ljf_aus@hotmail.com>
2025-07-08 14:22:57 +1000

    Completed

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-function playSound(event) {
-  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  let key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+const keys = Array.from(document.querySelectorAll(".key"));
+
+function playSound(e) {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
 }
 
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // skip if not transformed
+  this.classList.remove("playing");
+}
+
 window.addEventListener("keydown", playSound);
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
